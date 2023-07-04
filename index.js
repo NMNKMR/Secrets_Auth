df2282183628925c3eaa8e9bf12286f2c5199e65
NMNKMR <89262829+NMNKMR@users.noreply.github.com>
2023-07-04 23:59:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -82,8 +82,8 @@ passport.deserializeUser(function (user, cb) {
 });
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.clientID,
-    clientSecret: process.env.clientSecret,
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
